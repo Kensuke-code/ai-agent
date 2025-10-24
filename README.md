f28c054aee21d3e09cc2b8dc5d1cbecd32664f6e
Kensuke-code <kenzo71719083@gmail.com>
2025-10-25 00:13:12 +0900

    画面からstrandsに問い合わせて回答を表示するところまで確認

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ai-agent
-AIエージェント
+# AI-Agent
+## 概要
+- AIエージェントを管理するアプリケーションです
+- 以下の機能を提供します
+  - 
+
+## 構成図
+<img src="./ai-assistant.png">
--- strands/agent.py
+++ strands/agent.py
@@ -1,7 +1,41 @@
+# 必要なライブラリをインポート
+import os
 from dotenv import load_dotenv
 from strands import Agent
+from strands.tools import tool
+from tavily import TavilyClient
+from bedrock_agentcore.runtime import BedrockAgentCoreApp
 
+# .envファイルから環境変数をロード
 load_dotenv()
 
-agent = Agent()
-agent("日本の内閣総理大臣は？")
+# Define a tool to search the web
+@tool
+def search_tavily(query: str):
+    client = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
+    return client.search(query)
+
+# Strandsでエージェントを作成
+agent = Agent(
+    "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+    system_prompt="You are a helpful assistant that can search the web.",
+    tools=[search_tavily]
+)
+
+# AgentCoreのサーバーを作成
+app = BedrockAgentCoreApp()
+
+# エージェント呼び出し関数を、AgentCoreの開始点に設定
+@app.entrypoint
+def invoke_agent(payload, context):
+    # リクエストのペイロード（中身）からプロンプトを取得
+    prompt = payload.get("prompt")
+    
+    # エージェントを呼び出してレスポンスを返却
+    result = agent(prompt)
+
+    print(result.message["content"][0]["text"])
+    return {"result": result.message["content"][0]["text"]}
+
+# AgentCoreサーバーを起動
+app.run()
