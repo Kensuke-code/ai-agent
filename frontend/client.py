1fd420870abfb83e693e90cad581c78f4a2194eb
Kensuke-code <kenzo71719083@gmail.com>
2025-10-26 14:08:46 +0900

    会話履歴を表示できるよう修正

--- frontend/client.py
+++ frontend/client.py
@@ -10,24 +10,38 @@ load_dotenv(override=True)
 st.title("Kenchobi AI Agent")
 st.write("何でも聞いてね！")
 
+# チャットログを保存したセッション情報を初期化
+if "chat_log" not in st.session_state:
+    st.session_state.chat_log = []
+
 # チャットボックスを描画
 if prompt := st.chat_input("メッセージを入力してね"):
+
+    # 以前のチャットログを表示
+    for chat in st.session_state.chat_log:
+        with st.chat_message(chat["name"]):
+            st.write(chat["msg"])
 
     # ユーザーのプロンプトを表示
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # エージェントの回答を表示
-    with st.chat_message("assistant"):
 
-        # AgentCoreランタイムを呼び出し
-        with st.spinner("考え中…"):
-            agentcore = boto3.client('bedrock-agentcore')
-            response = requests.post(
-                url="http://strands:8080/invocations",
-                headers={"Content-Type": "application/json"},
-                json={"prompt": prompt}
-            )
+    # AgentCoreランタイムを呼び出し
+    with st.spinner("考え中…"):
+        agentcore = boto3.client('bedrock-agentcore')
+        response = requests.post(
+            url="http://strands:8080/invocations",
+            headers={"Content-Type": "application/json"},
+            json={"prompt": prompt}
+        )
 
-            response_body = response.json()["result"]
-            st.write(response_body)
+        result = response.json()["result"]
+
+        # エージェントの回答を表示
+        with st.chat_message("assistant"):
+          st.write(result)
+
+    # セッションにチャットログを追加
+    st.session_state.chat_log.append({"name": "user", "msg": prompt})
+    st.session_state.chat_log.append({"name": "assistant", "msg": result})
